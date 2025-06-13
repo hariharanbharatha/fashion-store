0bbf9ddba90c1b1a4c05e2459b94702e9fed95e3
Ubuntu <ubuntu@ip-172-31-25-75.ec2.internal>
2025-06-13 10:47:28 +0000

    Initial commit: fashion store app with Docker and Jenkins setup

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('👗 Welcome to Hariharan Fashion Store!');
+});
+
+app.get('/products', (req, res) => {
+  res.json([
+    { id: 1, name: 'T-Shirt', price: 499 },
+    { id: 2, name: 'Jeans', price: 999 },
+    { id: 3, name: 'Jacket', price: 1999 }
+  ]);
+});
+
+app.listen(3000, () => {
+  console.log('Fashion Store running on port 3000');
+});
+
