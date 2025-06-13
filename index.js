1becfbf716b1c6494d1031365fac2767c5991513
hariharanbharatha <hhariharan2525@gmail.com>
2025-06-13 08:22:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('👗 Welcome to Hariharan Fashion Store!');
+  res.send('👗 HARIHARANBHARATHA!');
 });
 
 app.get('/products', (req, res) => {
